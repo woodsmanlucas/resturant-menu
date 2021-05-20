da6b28aef14cf76ddd145492b7ec961ce392e689
Lucas Johnson <woodsman.lucas@gmail.com>
2021-05-20 05:19:06 -0700

    update breakfast page
    Added check to see if items are on the breakfast menu

--- src/components/breakfast.js
+++ src/components/breakfast.js
@@ -1,7 +1,38 @@
 import React from "react"
 import { graphql, StaticQuery } from "gatsby"
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import Grid from '@material-ui/core/Grid'
+import Button from '@material-ui/core/Button'
+import { useMediaQuery } from 'react-responsive';
+
+
+const useStyles = makeStyles({
+  root: {
+    width: 200,
+  },
+  media: {
+    height: 200,
+  },
+});
 
 export default function Breakfast(props) {
+  const classes = useStyles();
+  const isMobile = useMediaQuery({ query: '(max-device-width: 480px)' })
+  var flexDirection;
+  var gridWidth;
+  if (isMobile) {
+    flexDirection = "column"
+    gridWidth = 12
+  } else {
+    flexDirection = "row"
+    gridWidth = 4
+  }
+
   return (
     <StaticQuery
       query={graphql`
@@ -20,6 +51,9 @@ export default function Breakfast(props) {
                   id
                   images
                   name
+                  metadata {
+                    Menu
+                  }
                 }
               }
             }
@@ -27,14 +61,24 @@ export default function Breakfast(props) {
         }
       `}
       render={({ prices }) => (
-        <div>
+        <Grid container spacing={2} direction={flexDirection} justify="center" alignItems="center">
           {prices.edges.map(({ node: price }) => (
-            <div key={price.id}>
+            price.product.metadata.Menu === "Breakfast" &&
+            <Grid key={price.id} item xs={gridWidth}>
+            <Card className={classes.root}>
+              <CardActionArea>
+                {price.product.images && price.product.images.length > 0 && <CardMedia className={classes.media} image={price.product.images[0]} title={price.product.name}/>}
+                <CardContent>
                 <p>{price.product.name}</p>
-                {price.product.images && <img src={price.product.images} alt={price.product.name}/>}
-            </div>
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                <Button size="small" color="primary">Add to Cart</Button>
+              </CardActions>
+            </Card>
+            </Grid>
           ))}
-        </div>
+        </Grid>
       )}
     />
   )
